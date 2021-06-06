fa1c694d27970549af87412bb028599a60e744d1
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-06-06 12:16:07 +0300

    Added a simple backend server stub containing a single health end point.

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
 
 func main() {
-	fmt.Printf("Hello World!")
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello health!")
+	})
+
+	log.Printf("Starting a server at port %d", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
