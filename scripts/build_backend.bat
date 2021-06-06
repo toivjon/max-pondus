15d44298e96b3ef8c24ea3a2dd117c14e0ad3a6e
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-06-06 14:32:31 +0300

    Added the basis for the bat-script for building the backend.

---
+++ scripts/build_backend.bat
@@ -0,0 +1,33 @@
+:: This file builds the application backend from the sources.
+@echo off
+echo Building MaX Pondus backend
+
+:: Resolve the absolute path of the project root from the script path.
+set rootpath=%~dp0%
+set rootpath=%rootpath:~0,-9%
+
+:: Resolve and store the version of the Go environment.
+for /f "delims=" %%i in ('go version') do set goversion=%%i
+
+:: Resolve the folder where to put all build results.
+set buildpath=%rootpath%\bin\backend
+
+:: Resolve the folder which contains the main of the application.
+set cmdpath=%rootpath%\cmd\backend
+
+:: Show the information related to compilation environment.
+echo Detected environment information
+echo    Project root    %rootpath%
+echo    Build path      %buildpath%
+echo    Cmd path        %cmdpath%
+echo    Go version      %goversion%
+
+:: Remove the old build directory to ensure that we get a clean build.
+echo Removing the old build directory if it already exists
+if exist "%buildpath%" rd /s /q "%buildpath%"
+
+:: Gather and compile the backend sources files into an executable.
+echo Compiling the source files into an executable.
+mkdir %buildpath%
+cd %buildpath%
+go build -race %cmdpath%
