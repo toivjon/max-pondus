36c748f96f66f754c8a6541ff731a6e643c61985
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-08-29 07:43:02 +0300

    Modernized React initialization.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+
 import App from "./app.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Build and render the React application root structure.
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<React.StrictMode><App /></React.StrictMode>);
