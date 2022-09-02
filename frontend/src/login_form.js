3ddc7ebf7d2a39a1ddc950d4222bb4faf42154c6
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-02 07:35:46 +0300

    Added labels for the login form fields.

--- frontend/src/login_form.js
+++ frontend/src/login_form.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleSubmit(event) {
     const username = event.target.username.value;
     const password = event.target.password.value;
     console.log("Login username=" + username + " password=" + password);
+    event.preventDefault();
   }
 
   render() {
     return(
       <form className="login-form" onSubmit={this.handleSubmit}>
-        <input name="username" type="email"  placeholder="User Email" />
-        <br />
-        <input name="password" type="password" placeholder="Password" autoComplete="on" />
-        <br />
-        <button className="primary">Login</button>
+        <label htmlFor="username">Username</label>
+        <input id="username" name="username" type="email"  placeholder="User Email" />
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" placeholder="Password" autoComplete="on" />
+        <input name="submit" type="submit" value="Login" />
       </form>
     )
   }
