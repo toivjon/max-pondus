b7cf8a5f357c5cb858da623cbb27bca52d425858
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-03 16:04:33 +0300

    Simplified app import in the index.js.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './app.js';
+import App from './app';
 
 // Build and render the React application root structure.
 const rootElement = document.getElementById("root");
