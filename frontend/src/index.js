d0f395b8506103c694a02ab9d3bd3d3834e4d5c9
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-03 20:42:12 +0300

    Added a support for the react-router.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './app';
 
 // Import bootstrap styles to get UI components look pretty.
@@ -8,4 +9,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Build and render the React application root structure.
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
