500e0664cab225d097b66a8e74f054aa383983c6
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-03 16:21:08 +0300

    Added a documentation for the app class.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LoginForm from './login_form';
 import './app.css';
 
+// The application main entry point which builds the whole UI structure.
 export default class App extends Component {
   render(){
     return(
