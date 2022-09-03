35b45f449133298cc677c38763471c9f33e3891d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-03 20:18:23 +0300

    Changed App component into a function.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LoginForm from './login_form';
 import './app.css';
 
 // The application main entry point which builds the whole UI structure.
-export default class App extends Component {
-  render(){
-    return(
-      <div className="App">
-        <LoginForm />
-      </div>
-    );
-  }
+export default function App() {
+  return(
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
 }
