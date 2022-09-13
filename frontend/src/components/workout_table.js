943b495f27f9d5ba1df5717bbfb6aa22e7069291
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-13 07:18:11 +0300

    Added a stub model for workout.

--- frontend/src/components/workout_table.js
+++ frontend/src/components/workout_table.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import WorkoutTableItem from './workout_table_item';
+import Workout from '../models/workout';
 
 export default function WorkoutTable() {
-  const obj = {
-    date: "10.09.2022",
-    time: "09:30",
-    duration: "1 hour 30 min",
-    notes: "Foo seems to be bar here?"
-  };
+  const obj = new Workout();
   return (
     <Table>
       <thead>
---
+++ frontend/src/models/workout.js
@@ -0,0 +1,8 @@
+export default class Workout {
+  constructor() {
+    this.date = "10.09.2022";
+    this.time = "09:30";
+    this.duration = "1 hour 30 min";
+    this.notes = "Foo seems to be bar here?";
+  }
+}
