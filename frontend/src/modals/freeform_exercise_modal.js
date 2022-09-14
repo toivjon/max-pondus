26c676f76f07d799b34a420cc4a1e433cb049494
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-09-14 20:55:36 +0300

    Added couple placeholder fields into the freeform exercise layout.

--- frontend/src/modals/freeform_exercise_modal.js
+++ frontend/src/modals/freeform_exercise_modal.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Modal from 'react-bootstrap/Modal';
+
+// TODO Build a new workout instance and assign it as a state.
+// TODO Replace placeholders with the values from the workout state.
 
 export default function FreeformExerciseModal(props) {
   return (
@@ -8,7 +13,18 @@ export default function FreeformExerciseModal(props) {
         <Modal.Title>Freeform Exercise</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Hello Freeform!
+        <Form>
+          <InputGroup className='p-2'>
+            <InputGroup.Text>Date</InputGroup.Text>
+            <Form.Control type="date" value={"2022-10-10"}/>
+          </InputGroup>
+          <InputGroup className='p-2'>
+            <InputGroup.Text>Start</InputGroup.Text>
+            <Form.Control type="time" value={"09:10"}/>
+            <InputGroup.Text>End</InputGroup.Text>
+            <Form.Control type="time" value={"10:10"}/>
+          </InputGroup>
+        </Form>
       </Modal.Body>
     </Modal>
   );
